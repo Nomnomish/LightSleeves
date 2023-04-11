38ea36024d468a01bd814545fc4d4494c8a719c8
Jake Cannon <svenskakid12@gmail.com>
2023-04-11 14:27:35 -0700

    Actual Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<title>Lighter SleeveS: The only one you'll ever want</title>
+		</head>
+	<body>
+		<header id="header-part">
+			<img src="images/lighter-sleeves-logo.png" alt="Lighter SleeveS: The only one you'll ever want." />
+		</header>
+	</body>
+</html>
