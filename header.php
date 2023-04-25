0568a46fb7890f317d5707d67a03688977d89826
Jake Cannon <svenskakid12@gmail.com>
2023-04-25 16:43:57 -0600

    Mid-work safety commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+	<!--[if lt IE 7]><html lang="en-US" class="ie6"><![endif]-->
+	<!--[if IE 7]><html lang="en-US" class="ie7"><![endif]-->
+	<!--[if IE 8]><html lang="en-US" class="ie8"><![endif]-->
+	<!--[if IE 9]><html lang="en-US" class="ie9"><![endif]-->
+	<!--[if gt IE 9]><html lang="en-US"><![endif]-->
+	<!--[if !IE]><html lang="en-US"><![endif]-->
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Lighter SleeveS: The only one you'll ever want</title>
+		<link rel="stylesheet" href="styles.css">
+		<script src="https://kit.fontawesome.com/f9122f7dc0.js" crossorigin="anonymous"></script>
+	</head>
+		<body>
+		<header id="header-part">
+			<a href="https://lightersleeves.com" alt="Return to Lighter SleeveS Home"><img src="images/lighter-sleeves-logo.png" alt="Lighter SleeveS: The only one you'll ever want." class="logo" /></a>
+		</header>
+		<nav class="navbar">
+			<ul>
+				<li><a href="https://lightersleeves.com/" alt="Home">Home</a></li>
+				<li><a href="/newsletter">Newsletter Signup</a></li>
+				<li><a href="/shop">Our Shop</a></li>
+				<li>Social Media</li>
+				<li><ul>
+					<li><a href="https://twitter.com/LighterSleeves">Twitter</a></li>
+					<li><a href="https://www.facebook.com/lightersleeves">Facebook</a></li>
+				</ul></li>
+			</ul>
+		</nav>
